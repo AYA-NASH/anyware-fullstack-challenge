32c951df108649a33fd1c013330a303fd8412834
AYA_NASH <aya.nashaatx@gmail.com>
2025-07-21 19:46:55 +0300

    frontend set up

---
+++ frontend-react/src/App.tsx
@@ -0,0 +1,14 @@
+import Counter from "./Counter";
+import { Button } from "@mui/material";
+function App() {
+    return (
+        <div>
+            <h1> Anyware Task</h1>
+            <Button variant="contained">Hello World</Button>
+            <h2>Redux Tutorial</h2>
+            <Counter />
+        </div>
+    );
+}
+
+export default App;
